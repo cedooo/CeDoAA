db4d0fffd1a68dce0961abf8a259d03cf0ce086b
cedo <cedoo@qq.com>
2018-05-22 23:39:00 +0800

    添加风扇控制服务启动脚本

---
+++ HA/散热风扇温控/setup.sh
@@ -0,0 +1,10 @@
+cp fan-control.service /usr/lib/systemd/system/fan-control.service
+
+yum -y install python-devel
+cd /cedo
+wget https://files.pythonhosted.org/packages/e2/58/6e1b775606da6439fa3fd1550e7f714ac62aa75e162eed29dbec684ecb3e/RPi.GPIO-0.6.3.tar.gz \
+	&& tar zvf RPi.GPIO-0.6.3.tar.gz \
+	&& cd RPi.GPIO-0.6.3 \
+	&& python setup.py install
+
+systemctl enable homeassistant.service
