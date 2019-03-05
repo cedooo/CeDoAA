02c65730ab4484a0ce7e73feb9c9576e67db751d
cedoo <cedoo@qq.com>
2019-03-05 13:36:47 +0800

    调整结构

---
+++ HA/Shadowsocks/ssserver/ss-server-install.sh
@@ -0,0 +1,7 @@
+
+
+# https://my.vultr.com/affiliate/
+
+wget ¨Cno-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh
+chmod +x shadowsocks.sh
+./shadowsocks.sh 2>&1 | tee shadowsocks.log
---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+yum install git -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+yum install -y net-tools
