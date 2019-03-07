5d563d9ffd38a28abd88a7b3b160b2b707920103
cedoo <cedoo@qq.com>
2019-03-07 10:22:45 +0800

    add test for webhook

--- HA/Docker/flashdemo/app.py
+++ HA/Docker/flashdemo/app.py
@@ -25,5 +25,9 @@ def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
+@app.route('/cedoci')
+def hello():
+    return 'the git push!\n'
+	
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
