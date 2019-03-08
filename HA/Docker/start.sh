ac76bc64e58f0d210ffe005e1173378c81a6d2bc
cedoo <cedoo@qq.com>
2019-03-08 16:10:51 +0800

    启动脚本修改

--- HA/Docker/start.sh
+++ HA/Docker/start.sh
@@ -2,20 +2,23 @@
 
 #创建工作目录
 cedopath = "/cedo/"
-cedohasspath = "/cedo/homeassistant/"
-if [ ! -d "$cedopath"]; then 
+cedohasspath = "${cedopath}homeassistant/"
+if [ ! -d "$cedopath"] then 
+echo "创建工作目录"
 mkdir "$cedopath" 
 fi 
 
 #准备homeassistant配置文件
-if [ ! -d "$cedohasspath"]; then 
+if [ ! -d "$cedohasspath"] then 
+echo "准备homeassistant配置文件"
 mkdir "$cedohasspath"  
 cp homeassistant/configuration.yaml $cedohasspath
 cp homeassistant/customize.yaml $cedohasspath
 fi 
 
 #安装docker-compose
-if [ ! -f "/usr/local/bin/docker-compose" ]; then 
+if [ ! -f "/usr/local/bin/docker-compose" ] then 
+echo "安装docker-compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0-rc1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 fi 
